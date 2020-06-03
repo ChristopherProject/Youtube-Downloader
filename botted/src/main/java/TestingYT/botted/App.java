3620edc2c80bbe6e67416ad3e889566ca1044595
Chris <61153610+ChristopherProject@users.noreply.github.com>
2020-06-03 08:46:42 +0200

    Update App.java

--- botted/src/main/java/TestingYT/botted/App.java
+++ botted/src/main/java/TestingYT/botted/App.java
@@ -1,29 +1,87 @@
 package TestingYT.botted;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 /**
  * Hello world!
  *
  */
 public class App {
-
+	
 	public static void main(String[] args) {
-		System.out.println("Inserisci Il Link Della Canzone Da Scaricare:");
-		Scanner in = new Scanner(System.in);
-		String link_da_scaricare = in.nextLine();
-		System.out.println("Inserisci Il Nome Che Vuoi Dare Al File");
-		Scanner in2 = new Scanner(System.in);
-		String nome_song = in2.nextLine();
-		DonwloadFileFromYT(link_da_scaricare, nome_song);
+		System.out.println("Programma Avviato =)");
+		SaveYouTubeMP3ToFile("https://www.youtube.com/watch?v=WWRy1zT-CjI");
+	}
+	
+	/**
+	 * @note New Method For Downloading Music.
+	 * @param URL_YOUTUBE_VIDEO
+	 */
+	
+	public static void SaveYouTubeMP3ToFile(String URL_YOUTUBE_VIDEO) {
+		JFileChooser chooser = new JFileChooser();
+		chooser.setDialogTitle("Select Dir To Save mp3");
+		chooser.setCurrentDirectory(new File(System.getenv("USERNAME") + File.separator + "Deskstop"));
+		int retrival = chooser.showOpenDialog(chooser);
+		String url = URL_YOUTUBE_VIDEO;
+		String video_id = url.replace("https://www.youtube.com/watch?v=", "");
+		String diocane = video_id.replace(" ", "");
+		String url_complete = "http://cdn.audioham.net/api/v2/download?videoId=" + diocane + "&itag=140&name=" + chooser.getSelectedFile() + ".mp3";
+		URL url_2;
+		if (retrival == JFileChooser.APPROVE_OPTION) {
+			try {
+				try {
+					System.out.println("Richiesta Inviata All' API");
+					url_2 = new URL(url_complete);
+					InputStream in;
+					ByteArrayOutputStream by_arr;
+					int max_data = 8224;
+					try {
+						in = url_2.openStream();
+						by_arr = new ByteArrayOutputStream(max_data);
+						System.out.println("Copiando I File Data Dall API...");
+						int length = -1;
+						byte[] buffer = new byte[max_data];
+						while ((length = in.read(buffer)) > -1) {
+							by_arr.write(buffer, 0, length);
+						}
+						by_arr.close();
+						in.close();
+						try (FileOutputStream fw = new FileOutputStream(chooser.getSelectedFile() + ".mp3")) {
+							System.out.println("Copiando I Byte Sul File Locale...");
+							fw.write(by_arr.toByteArray());
+							System.out.println("File Scaricato in " + chooser.getSelectedFile().getPath().toString() + ".mp3");
+						} catch (Exception ex1) {
+							System.out.println("Download Dei Byte Del File Non Riuscito");
+						}
+						System.out.println("File Scaricato Correttamente");
+					} catch (IOException e) {
+						System.out.println("Download Non Riuscito");
+					}
+				} catch (MalformedURLException e) {
+					System.out.println("La Sintassi Della URL è Errata!");
+				}
+
+			} catch (Exception ex) {
+				System.out.println("Impossibile Inizializzare Il File");
+			}
+		}
 	}
 
+	/**
+	 * @note Olded Method
+	 * @param YOUTUBE_LINKED
+	 * @param TITLE_OF_SONG
+	 */
+	
 	public static void DonwloadFileFromYT(String YOUTUBE_LINKED, String TITLE_OF_SONG) {
 		String url = YOUTUBE_LINKED;
 		String video_id = url.replace("https://www.youtube.com/watch?v=", "");
